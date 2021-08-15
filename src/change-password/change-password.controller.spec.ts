fc2dd74baffe945a22e856b26f3985dad675d629
Bruno <brunofelipedk@gmail.com>
2021-08-15 20:22:23 -0300

    Testes autenticação, atualizar senha

--- src/change-password/change-password.controller.spec.ts
+++ src/change-password/change-password.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Users } from 'src/users/entities/users.entity';
+import { UserRole } from 'src/users/user-roles.enum';
 import { ChangePasswordController } from './change-password.controller';
+import { ChangePasswordService } from './change-password.service';
 
-describe('ChangePassword Controller', () => {
-  let controller: ChangePasswordController;
+describe('Register Controller', () => {
+    let changePasswordController: ChangePasswordController;
+    let changePasswordService: ChangePasswordService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ChangePasswordController],
-    }).compile();
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [ChangePasswordController],
+        providers: [
+          {
+            provide: ChangePasswordService,
+            useValue: {
+              changePassword: jest.fn().mockResolvedValue(
+                new Users({id: 4, name: 'Sabrina Freitas', username: "sabrina", email: "sabrina@gmail.com", password: "123456", role: UserRole.ADMIN, createdAt: new Date(), updatedAt: new Date()})
+              ),
+            }
+          }
+        ]
+      }).compile();
 
-    controller = module.get<ChangePasswordController>(ChangePasswordController);
-  });
+      changePasswordController = module.get<ChangePasswordController>(ChangePasswordController);
+      changePasswordService = module.get<ChangePasswordService>(ChangePasswordService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+      expect(changePasswordController).toBeDefined();
+      expect(changePasswordService).toBeDefined();
+    });
+
+    describe('changePassword', () => {
+      it('should return a updated user successfully', async () => {
+        const change = {
+          "email": "sabrina@gmail.com",
+          "password": "123456"
+        }
+        const result = await changePasswordService.changePassword(change);
+        expect(result.email).toEqual('sabrina@gmail.com');
+      });
+    });
+
 });
